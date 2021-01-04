44447b93619c33996712da73e811ee793bf66b12
bhagath <66695416+bhagath348@users.noreply.github.com>
2021-01-03 22:02:49 -0500

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 import os
 from flask import flask
-app = Flask (__name__)
+app = Flask(__name__)
 
 @app.route("/")
 def main():
-    return(Welcome!)
+    return "Welcome!"
 
 @app.route('/how are you')
 def hello():
